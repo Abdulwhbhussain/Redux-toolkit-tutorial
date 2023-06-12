8dedc74dea3de7dd0871892304704c8393b6c2a4
WHB901 <abdulwhbhussain@gmail.com>
2023-06-12 23:31:25 +0500

    Working

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,7 +1,17 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
   return (
     <main>
       <Navbar />
--- starter/src/components/CartContainer.js
+++ starter/src/components/CartContainer.js
@@ -35,7 +35,7 @@ function CartContainer() {
         <hr />
         <div className='cart-total'>
           <h4>
-            total <span>${total}</span>
+            total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
         <button onClick={() => dispatch(clearCart())} className='btn clear-btn'>clear cart</button>
